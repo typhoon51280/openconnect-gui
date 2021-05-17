f1807ee42e5ec58c60173afd87166a31a85f491a
typhoon51280 <typhoon51280@users.noreply.github.com>
2021-05-17 08:07:49 +0200

    svelte App

--- main.go
+++ main.go
@@ -1,24 +1,60 @@
 package main
 
 import (
+	"embed"
+	"fmt"
+	"io/fs"
 	"log"
-	"net/url"
+	"net"
+	"net/http"
+	"os"
+	"os/signal"
+	"runtime"
+	"syscall"
 
 	"github.com/zserge/lorca"
 )
 
+//go:embed frontend/public
+var frontend_public embed.FS
+
 func main() {
-	// Create UI with basic HTML passed via data URI
-	ui, err := lorca.New("data:text/html,"+url.PathEscape(`
-	<html>
-		<head><title>Hello</title></head>
-		<body><h1>Hello, world!</h1></body>
-	</html>
-	`), "", 480, 320)
+	args := []string{}
+	if runtime.GOOS == "linux" {
+		args = append(args, "--class=Lorca")
+	}
+
+	// launch Chrome Instance (lorca)
+	ui, err := lorca.New("", "", 480, 320, args...)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer ui.Close()
-	// Wait until UI window is closed
-	<-ui.Done()
+
+	// listen on localhost random socket
+	server, err := net.Listen("tcp", "127.0.0.1:0")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer server.Close()
+
+	// serve frontend
+	serverRoot, err := fs.Sub(frontend_public, "frontend/public")
+	if err != nil {
+		log.Fatal(err)
+	}
+	go http.Serve(server, http.FileServer(http.FS(serverRoot)))
+
+	url := fmt.Sprintf("http://%s", server.Addr())
+	log.Println(url)
+	ui.Load(url)
+
+	// Wait until the interrupt signal arrives or browser window is closed
+	sigc := make(chan os.Signal, 1)
+	signal.Notify(sigc, syscall.SIGINT, syscall.SIGTERM)
+	select {
+	case <-sigc:
+	case <-ui.Done():
+	}
+	log.Println("Shutting down ...")
 }
